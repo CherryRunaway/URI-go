ea3ddae9f8fee1c1209864487233e35fb1e0b34f
Isadora Rocha <isadora.rocha.de.almeida@gmail.com>
2019-09-13 00:24:53 -0300

    Update 1002.go

--- 1002.go
+++ 1002.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-  var (
+  	var (
 		r float64
 		pi float64 = 3.14159
 	)
 	
 	fmt.Scan(&r)
-   area := pi * (r*r)
+   	area := pi * (r*r)
 	fmt.Printf("A=%.4f\n", area)
 }
